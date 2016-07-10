d1b40a92fdca71835d549dad34777b719af0efee
l08084 <l08084@shibaura-it.ac.jp>
2016-07-10 17:47:22 +0900

    add goodby!!

--- php-driver-hello/public/index.php
+++ php-driver-hello/public/index.php
@@ -1,9 +1,21 @@
 <?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = new \Slim\App();
-$app->get('/hello/{name}', function ($request, $response, $args) {
-    $response->getBody()->write("Hello, ".$args['name']);
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello!!!, $name");
+    return $response;
+    //echo 'Hello!!!, ', $name;
+});
+$app->get('/goodby/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("goodby!!!, $name");
     return $response;
 });
+
+
 $app->run();
